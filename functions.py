8cfafb0d5a87cacc1e2e8eb31c8d0d1ae66be04a
jensmdriller <jensdriller@gmail.com>
2018-01-13 09:48:13 -0800

    added single-select and multi-select from lists. Still working on error catching and input validation

--- functions.py
+++ functions.py
@@ -1,7 +1,47 @@
+def single_select_from_list(choice_list):
+    #global user_sel #dont use global... too dangerous
+    for i,each in enumerate(choice_list):
+        print("Option [%i]: %s" %(i+1,each))
+    user_sel_str = input("\nPlease select one an option from the list above or press 'q' to quit\n--> ")
+    if user_sel_str.lower().replace(" ","") == 'q':
+        print("Exiting...\n")
+        return None
+    try:
+        user_sel = int(user_sel_str)-1
+        if user_sel not in range(len(choice_list)):
+            raise Exception('Value out of range') 
+    except:
+        print("\nYou have entered an invalid option, please try again or press 'q' to quit\n")
+        user_sel, choice = single_select_from_list(choice_list)
+    return user_sel, choice_list[user_sel]
+
+def multi_select_from_list(choice_list):
+    import re
+    for i,each in enumerate(choice_list):
+        print("Option [%i]: %s" %(i+1,each))
+    user_sel_str = input("\nPlease select one an option from the list above or press 'q' to quit\n--> ")
+    if user_sel_str.lower().replace(" ","") == 'q':
+        print("Exiting...\n")
+        return None
+    remove_whitspaces = user_sel_str.replace(" ","")
+    seperate_by_comma = remove_whitspaces.split(',')
+    selection_array = [re.sub("\D", "", each) for each in seperate_by_comma]
+    try:
+        selection_array.remove('') #remove empty entries
+    except:
+        pass
+    for each_selection in selection_array:
+        user_sel = int(each_selection)-1
+        if user_sel not in range(len(choice_list)):
+            print("\nEntry [%s] is an invalid option, please try again or press 'q' to quit\n" %(int(user_sel)+1))
+            multi_select_from_list(choice_dict);break
+    else:
+        return [choice_list[int(each_sel)-1] for each_sel in selection_array]
+
 def single_select_from_dict(choice_dict):
     list_keys=list(choice_dict.keys())
     for i,each in enumerate(list_keys):
-        print("Selection [%i]: %s %s" %(i+1,each,choice_dict[each]))
+        print("Option [%i]: Key:[%s] Value:[%s]" %(i+1,each,choice_dict[each]))
     user_sel_str = input("\nPlease select one an option from the list above or press 'q' to quit\n--> ")
     if user_sel_str.lower().replace(" ","") == 'q':
         print("Exiting...\n")
@@ -9,14 +49,14 @@ def single_select_from_dict(choice_dict):
     user_sel = int(user_sel_str)-1
     if user_sel not in range(len(list_keys)):
         print("\nYou have entered an invalid option, please try again or press 'q' to quit\n")
-        multi_select_dict(choice_dict)
+        single_select_from_dict(choice_dict)
     return {list_keys[user_sel]:choice_dict[list_keys[user_sel]]}
 
 def multi_select_from_dict(choice_dict):
     import re
     list_keys=list(choice_dict.keys())
     for i,each in enumerate(list_keys):
-        print("Selection [%i]: %s %s" %(i+1,each,choice_dict[each]))
+        print("Option [%i]: %s %s" %(i+1,each,choice_dict[each]))
     user_sel_str = input("\nPlease select one an option from the list above or press 'q' to quit\n--> ")
     if user_sel_str.lower().replace(" ","") == 'q':
         print("Exiting...\n")
@@ -32,6 +72,6 @@ def multi_select_from_dict(choice_dict):
         user_sel = int(each_selection)-1
         if user_sel not in range(len(list_keys)):
             print("\nEntry [%s] is an invalid option, please try again or press 'q' to quit\n" %(int(user_sel)+1))
-            multi_select_dict(choice_dict);break
+            multi_select_from_dict(choice_dict);break
     else:
         return {list_keys[int(each_sel)-1]:choice_dict[list_keys[int(each_sel)-1]] for each_sel in selection_array}
